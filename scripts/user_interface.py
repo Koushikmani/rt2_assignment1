8cf79e5fb8e248d9de9058220724dd98377c8fac
Koushikmani <81651764+Koushikmani@users.noreply.github.com>
2022-03-06 17:52:41 +0100

    Update user_interface.py

--- scripts/user_interface.py
+++ scripts/user_interface.py
@@ -4,18 +4,21 @@ from rt2_assignment1.srv import Command
 
 def main():
     rospy.init_node('user_interface')
+    rospy.wait_for_service('/user_interface')
     ui_client = rospy.ServiceProxy('/user_interface', Command)
-    time.sleep(10)
+    time.sleep(5)
     rate = rospy.Rate(20)
     x = int(input("\nPress 1 to start the robot "))
     while not rospy.is_shutdown():
         if (x == 1):
+            print("Robot starting")
             ui_client("start")
             x = int(input("\nPress 0 to stop the robot "))
         else:
-            print("Please wait, the robot is going to stop when the position will be reached")
+            print("Robot stopping")
             ui_client("stop")
             x = int(input("\nPress 1 to start the robot "))
+#-#-#-#-#
             
 if __name__ == '__main__':
     main()
